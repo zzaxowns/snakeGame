ee3a89b6c0559c7dbe9b90f319a9fac903b01e28
KimTaeJun <zzaxowns@naver.com>
2021-12-12 14:21:56 +0900

    input 관련 클래스 제작 중
    테스트 중

--- SnakeGame/input.cpp
+++ SnakeGame/input.cpp
@@ -1,1 +1,36 @@
 #include "input.h"
+
+input::input():object(x,y) {
+}
+
+input::~input() {
+}
+
+void input::update() {
+	char key;
+
+	if (_kbhit()) {
+		key = _getch();
+
+		if (key == -32) {    // -32로 입력되면
+			key = _getch();  // 새로 입력값을 판별하여 상하좌우 출력
+			switch (key) {
+			case LEFT:
+				printf("왼쪽으로 이동\n");
+				break;
+			case RIGHT:
+				printf("오른쪽으로 이동\n");
+				break;
+			case UP:
+				printf("위로 이동\n");
+				break;
+			case DOWN:
+				printf("아래로 이동\n");
+				break;
+			}
+		}
+	}
+}
+
+void input::draw() {
+}
--- SnakeGame/input.h
+++ SnakeGame/input.h
@@ -2,8 +2,19 @@
 #include <conio.h>
 #include "object.h"
 
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
 class input : public object
 {
+public:
+	input();
+	~input();
 
+public:
+	void update() override;
+	void draw() override;
 };
 
