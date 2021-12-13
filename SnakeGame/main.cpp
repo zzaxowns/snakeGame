283cc2464d69f02d86157febb6cd467ad924affd
KimTaeJun <zzaxowns@naver.com>
2021-12-13 18:16:57 +0900

    화면 지우기 추가

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <windows.h>
+
 #include "object.h"
 #include "map.h"
 using namespace std;
@@ -8,6 +10,7 @@ int main(){
 	object* maps = new map(25,50,0,0);
 
 	maps->draw();
+	//system("cls"); //화면 지우기
 
 	delete maps;
 	return 0;
