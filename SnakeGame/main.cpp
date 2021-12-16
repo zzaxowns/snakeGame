86ae4bd7d72d87d6d13f25f1b1e787af69c5232f
KimTaeJun <zzaxowns@naver.com>
2021-12-16 19:02:44 +0900

    우선 모든 부분에 전부 써보고 나중에 고칠 생각

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -3,82 +3,67 @@
 #include <conio.h>
 //============================
 
-#include "object.h"
-#include "snake.h"
-#include "moveCommand.h"
-
-#define UP 72
-#define DOWN 80
-#define LEFT 75
-#define RIGHT 77
-#define HEIGHT 25
+#define HEIGHT 20
 #define WIDTH 20
 
 using namespace std;
 
 int map[HEIGHT][WIDTH];
+bool gameOver;
 
-command* move_Command[4];
-object* player;
-int main(){
-	
-	player = new snake(10, 10);
+int x, y, fruit_X, fruit_Y, score;
 
-	move_Command[0] = new moveCommand(player, 0, -1); //Up
-	move_Command[1] = new moveCommand(player, 0, 1);  //Down
-	move_Command[2] = new moveCommand(player, -1, 0); //Left
-	move_Command[3] = new moveCommand(player, 0, 1);  //Right
+enum Direction {
+	STOP = 0,
+	LEFT, 
+	RIGHT, 
+	UP, 
+	DOWN
+};
 
+Direction dir;
 
-	while (1) {
-
-		//초기화
-		init_map();
-
-		//업데이트
-		update();
-
-		//그리기
-		draw();
-		
-		//지우기
-		Sleep(100);
-		system("cls"); //화면 지우기
-		Sleep(100);
-	}
-
-
-
-	delete player;
-	delete move_Command;
-
-	return 0;
+void Setup() {
+	gameOver = false;
+	dir = STOP;
+	x = WIDTH / 2;
+	y = HEIGHT / 2;
+	fruit_X = rand() % WIDTH;
+	fruit_Y = rand() % HEIGHT;
+	score = 0;
 }
 
-void update() { 
-	input(); // 키 입력
-	map[player->x][player->y] = '0';
-}
+void Draw() {
+	system("cls");
 
-void init_map() { 
+	for (int i = 0; i < WIDTH; i++)
+		cout << "#";
+	cout << endl;
+
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
-			if (i == 0 || j == 0 || i == HEIGHT - 1 || j == WIDTH - 1)
-				map[i][j] = '#';
-		}
-	}
-}
+			if (j == 0 || j == WIDTH - 1)
+				cout << "#";
+			else
+				cout << " ";
 
-void draw() {
-	for (int i = 0; i < HEIGHT; i++) {
-		for (int j = 0; j < WIDTH; j++) {
-			cout << map[i][j];
+			if (i == y && j == x)
+				cout << "O";
+			else if (i == fruit_Y && j == fruit_X)
+				cout << "F";
+			else
+				cout << " ";
 		}
 		cout << endl;
 	}
+
+
+	for (int i = 0; i < WIDTH; i++)
+		cout << "#";
+	cout << endl;
 }
 
-void input() {
+void Input() {
 	char key;
 
 	if (_kbhit()) {
@@ -88,18 +73,38 @@ void input() {
 			key = _getch();  // 새로 입력값을 판별하여 상하좌우 출력
 			switch (key) {
 			case LEFT:
-				move_Command[2]->excute();
+
 				break;
 			case RIGHT:
-				move_Command[3]->excute();
+
 				break;
 			case UP:
-				move_Command[0]->excute();
+
 				break;
 			case DOWN:
-				move_Command[1]->excute();
+
 				break;
 			}
 		}
 	}
 }
+
+void Logic() {
+
+}
+
+int main(){
+
+	Setup();
+
+	while (!gameOver) {
+		Draw();
+		Input();
+		Logic();
+
+	}
+
+	return 0;
+}
+
+
